9b24fe8817bfb4a0c258a2a47f1693e0a10d29cb
Nusaiba Faiza <nusaiba.faiza@gmail.com>
2024-09-27 19:14:32 +0600

    first commit

---
+++ Check Frequency.py
@@ -0,0 +1,19 @@
+test_dict={
+    'codingal': 3,
+    'is': 2,
+    'best': 2,
+    'for': 2,
+    'Codingal': 1
+}
+
+print("The original dictionary: ", test_dict)
+
+K = int(input("Enter the frequency: "))
+
+res = 0
+
+for key in test_dict:
+    if test_dict[key]==K:
+        res = res+1
+        
+print("Frequency of K is: ", res)
