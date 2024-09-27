6c61e971149c9daf27e86acc5ecac46ff4dee118
Nusaiba Faiza <nusaiba.faiza@gmail.com>
2024-09-27 19:38:05 +0600

    >.<

---
+++ country code.py
@@ -0,0 +1,15 @@
+country_code = {
+    'India': '0091',
+    'Austrilia': '0025',
+    'Bangladesh': '00880',
+    'Nepal': '00977'
+}
+
+print("Country code for India -")
+print(country_code.get('India','Not found'))
+
+print("Country code for Bang;ladesh -")
+print(country_code.get('Bangladesh','Not found'))
+
+print("Country code for Japan -")
+print(country_code.get('Japan','Not found'))
